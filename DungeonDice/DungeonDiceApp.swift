9aaee9a30d9c936cac1ce6d4f887a1e2f2396763
codeVillier <odevill@icloud.com>
2026-02-24 18:05:07 +0200

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Oscar De Villiers on 2026/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
